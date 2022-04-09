533988864ae8c5a9fe123d963fbf96f1e994da03
Elias Myronidis <elias.myronidis@hotmail.com>
2022-04-09 19:01:51 +0300

    Add tabs

--- UltimatePortfolio/ContentView.swift
+++ UltimatePortfolio/ContentView.swift
@@ -9,13 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView {
+            HomeView()
+                .tabItem {
+                    Label("Home", systemImage: "house")
+                }
+            
+            ProjectsView(showClosedProjects: false)
+                .tabItem {
+                    Label("Open", systemImage: "list.bullet")
+                }
+            
+            ProjectsView(showClosedProjects: true)
+                .tabItem {
+                    Label("Closed", systemImage: "checkmark")
+                }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
+    static let dataController = DataController.preview
+    
     static var previews: some View {
         ContentView()
+            .environment((\.managedObjectContext), dataController.container.viewContext)
+            .environmentObject(dataController)
     }
 }
---
+++ UltimatePortfolio/HomeView.swift
@@ -0,0 +1,31 @@
+//
+//  HomeView.swift
+//  UltimatePortfolio
+//
+//  Created by Elias Myronidis on 9/4/22.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    @EnvironmentObject var dataController: DataController
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                Button("Add Data") {
+                    dataController.deleteAll()
+                    try? dataController.createSampleData()
+                }
+            }
+            
+            .navigationTitle("Home")
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
